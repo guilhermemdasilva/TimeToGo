d9193cac501ba527575f0a5fb2f47cac186e60b3
Guilherme Monteiro <guilherme.mdasilva@gmail.com>
2016-04-11 15:16:02 -0400

    remove useless permissions

--- app/src/main/java/com/guilermetell/timetogo/HomeActivity.java
+++ app/src/main/java/com/guilermetell/timetogo/HomeActivity.java
@@ -1,10 +1,6 @@
 package com.guilermetell.timetogo;
 
-import android.content.Context;
 import android.content.DialogInterface;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Vibrator;
 import android.preference.PreferenceManager;
 import android.support.v4.app.DialogFragment;
 import android.os.Bundle;
@@ -14,8 +10,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-
-import java.util.Calendar;
 
 public class HomeActivity extends FragmentActivity implements TimePickerDialogCloseListener {
 
